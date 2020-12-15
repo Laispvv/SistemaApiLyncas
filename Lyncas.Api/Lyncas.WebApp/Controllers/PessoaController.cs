5479db57802a695a0400b130fe051d4a2b0e25a9
Lais <lais.vossen@gmail.com>
2020-12-15 10:24:27 -0300

    adicionando as views

---
+++ Lyncas.Api/Lyncas.WebApp/Controllers/PessoaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lyncas.WebApp.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
